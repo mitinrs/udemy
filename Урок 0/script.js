b87321935a11fac512d5ac55f61724f381d1bf11
Roman Mitin <mitin@graviongroup.ru>
2020-04-05 08:45:46 +0300

    add files

---
+++ Урок 0/script.js
@@ -0,0 +1,18 @@
+let person={
+    Name:"Roma",
+    Age:37,
+    Married:true};
+
+
+console.log(person);
+
+let arr=['qq','ww','dd'];
+console.log(arr);
+
+// alert('HW')
+
+// let a=confirm('q1');
+// console.log(a);
+
+let a=prompt('q1','Да');
+console.log(a);
